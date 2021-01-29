68a9a5589bb2bd5e5d04e70bf3c2598435ed2321
Brianrahmarela <brian.rahmarela@gmail.com>
2021-01-29 12:24:15 +0700

    update soal no 6 file index.html dan soalno6.js

--- soalno6.js
+++ soalno6.js
@@ -1,16 +1,19 @@
 //buat array kosong utk menampung hasil registrasi
 let registrasi = [];
 
-//buat fungsi utk menampilkan hasil tweet
+//ambil id elemen yg akan menampilkan hasilnya & assign variable
+let listItem = document.getElementById("listitem");
+
+//buat fungsi utk menampilkan hasil registrasi
 function displayForm() {
     //ambil id nama & password
     let nama = document.getElementById("nama").value;
     let pass = document.getElementById("password").value;
-    //cek apakah value textarea yg diinput user dapat
+    //cek apakah value registrasi yg diinput user dapat
     // console.log(nama);
     // console.log(pass);
 
-    //acak password inputan user dgn Math.random
+    //acak password (variable pass) inputan user dgn Math.random
     let hashPass = Math.random(pass);
 
     // buat validasi cek nama & password
@@ -21,23 +24,33 @@ function displayForm() {
     } else if (pass.length < 8) {
         alert("Password must be at least 8 characters long!")
     } else {
-        //panggil variable hashing jika validasi lolos 
+        //panggil variable hashing jika validasi lolos & tampung di variable registrasi
         registrasi.push(nama, hashPass)
+        // listItem.innerHTML += registrasi[registrasi.length-2]+' '+registrasi[registrasi.length-1] + "\n";
+
+        //tampilkan
+        /* Di variable listItem tempelkan dgn .innerhtml utk mendapakan value. assign nilai di variabel registrasi & ambil index yg ingin ditampilkan [registrasi.length-2] artinya ambil index registrasi yg lengthnya yg jmlhnya 2 index dikurang 2 (.length-2). karena ada 2 index di array registrasi, maka hasil length-2=0. artnya ambil index ke [0] yaitu nama. length-1 berarti ambil jmlh length-1=1. yaitu ambil index ke [1] yaitu password */
+        listItem.innerHTML +=`<p>${registrasi[registrasi.length-2]}</p>`; //value array nama
+        listItem.innerHTML +=`<p>${registrasi[registrasi.length-1]}</p>`; //value array password
+        //kosongkan value inputan nama & password
+        document.getElementById("nama").value = '';
+        document.getElementById("password").value = '';
     }
+};
+
 
     //looping array & buat array baru
-    let listRegis = registrasi.map((value) => {
-        // console.log(value);
-        return value;
-        // return value.split(',',2);
-    });
+    // let listRegis = registrasi.map((value) => {
+    //     // console.log(value);
+    //     return value;
+    //     // return value.split(',',2);
+    // });
 
-    //tampil nama
-    document.getElementById("hasilnama").innerHTML = listRegis[0];
-    document.getElementById("nama").value = '';
+    // //tampil nama
+    // document.getElementById("hasilnama").innerHTML = listRegis[0];
+    // document.getElementById("nama").value = '';
 
-    //tampil password
-    document.getElementById("hasilpass").innerHTML = listRegis[1];
-    document.getElementById("password").value = '';
+    // //tampil password
+    // document.getElementById("hasilpass").innerHTML = listRegis[1];
+    // document.getElementById("password").value = '';
     
-};
